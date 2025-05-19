c34d840aa4d544ceeb3a42abbe3f539522797df8
richnou <leys.richard@gmail.com>
2025-05-19 14:55:56 +0200

    Initial commit based on tree sitter default TCL grammar

---
+++ languages/tcl/indents.scm
@@ -0,0 +1,30 @@
+;[
+; (braced_word_simple)
+; (namespace)
+; (command)
+; (if)
+; (else)
+; (elseif)
+; (foreach)
+; (while)
+; (try)
+; (procedure)
+; (command_substitution)
+; ] @indent.begin
+
+;(quoted_word) @indent.align
+
+;([
+;  (expr)
+;  (arguments)
+;] @indent.align
+;  (#set! "indent.open_delimiter" "{")
+;  (#set! "indent.close_delimiter" "}"))
+
+[
+  "}"
+  "]"
+] @end
+
+
+;(if "}" @end) @indent
